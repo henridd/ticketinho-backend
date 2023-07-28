a79153acda4341ee781b9525173711304a57e844
Thiago <thiagokoster@gmail.com>
2023-07-28 15:31:12 +0200

    Rename solution and project to match c# naming style

--- Functions/AuthFunctions.cs
+++ Functions/AuthFunctions.cs
@@ -15,14 +15,14 @@ namespace Ticketinho
         }
 
         [Function("Login")]
-        public HttpResponseData Login([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public HttpResponseData Login([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-
+            
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Welcome to Azure Functions!!");
 
             return response;
         }
