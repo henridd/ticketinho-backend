0346653ddb7ba233f52781b8077ad98261dc82c5
henridd <henri.oo@hotmail.com>
2023-08-13 18:39:57 +0200

    Create boostrap project

---
+++ Ticketinho.Bootstrap/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ticketinho.Bootstrap
+{
+    public static class Bootstrapper
+    {
+        public static void Start(IServiceCollection services)
+        {
+            Ticketinho.Repository.Bootstrapper.RegisterServices(services);
+            Ticketinho.Service.Bootstrapper.RegisterServices(services);
+        }
+    }
+}
