f88620a287017cc6142a4e5f32c14da866a5bd5c
henridd <henri.oo@hotmail.com>
2023-09-03 15:19:39 +0200

    Add missing JsonPropertyName for DTOs

--- Ticketinho.Common/DTOs/Demand/CreateDemandRequestDto.cs
+++ Ticketinho.Common/DTOs/Demand/CreateDemandRequestDto.cs
@@ -5,12 +5,16 @@ namespace Ticketinho.Common.DTOs.Demand
 {
     public class CreateDemandRequestDto
     {
+        [JsonPropertyName("userId")]
         public string UserId { get; set; }
 
+        [JsonPropertyName("price")]
         public double Price { get; set; }
 
+        [JsonPropertyName("zone")]
         public TicketZone Zone { get; set; }
 
+        [JsonPropertyName("type")]
         public TicketType Type { get; set; }
     }
 }
--- Ticketinho.Common/DTOs/Negotiation/CreateNegotiationRequestDto.cs
+++ Ticketinho.Common/DTOs/Negotiation/CreateNegotiationRequestDto.cs
@@ -1,13 +1,17 @@
-﻿using Ticketinho.Common.Enums;
+﻿using System.Text.Json.Serialization;
+using Ticketinho.Common.Enums;
 
 namespace Ticketinho.Common.DTOs.Negotiation
 {
     public class CreateNegotiationRequestDto
     {
+        [JsonPropertyName("ticketId")]
         public required string TicketId { get; set; }
 
+        [JsonPropertyName("sellerId")]
         public required string SellerId { get; set; }
 
+        [JsonPropertyName("buyerId")]
         public required string BuyerId { get; set; }
     }
 }
